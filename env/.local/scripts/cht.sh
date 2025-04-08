4f551f47872ac64e168f5f2cf4b71dedba6a2461
Eric Milan <emilan@ericmilan.dev>
2025-04-08 18:41:11 -0400

    Fixed colors

---
+++ env/.local/scripts/cht.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+languages=`echo "golang lua cpp c rust zig python" | tr " " "\n"`
+core_utils=`echo "xargs find mv sed awk grep tree ls cd rm" | tr " " "\n"`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+read -p "query: " query
+
+if printf "$languages" | grep -qs $selected; then
+    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+else
+    curl cht.sh/$selected~$query
+fi
